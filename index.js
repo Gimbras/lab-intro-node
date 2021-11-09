55cd9a94070a527e0d36b181eabf8f685fd86b1e
Gimbras <92314794+Gimbras@users.noreply.github.com>
2021-11-09 18:19:01 +0000

    lab complete
    Rodrigo and Yulia

--- index.js
+++ index.js
@@ -1,17 +1,71 @@
 class SortedList {
-  constructor() {}
+  constructor(items, lenght) {
+     this.items = [];
+     this.length = 0; 
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) => {
+      if (a < b) {
+          return 1
+      } else if (a > b) {
+          return -1
+      } else {
+          return 0
+      }
+      //return this.items
+    })
+  }
 
-  max() {}
+  get(pos) {
+    value = this.items[pos]
+        if (value !== 'undefinied') {return value}
+        else {throw new Error('OutOfBounds')}
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+        throw new Error('EmptySortedList')
+} else {
+return Math.max(...this.items)
+}
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+    throw new Error('EmptySortedList')
+} else {
+return Math.min(...this.items)
+}}
 
-  avg() {}
+sum() {
+          let sum = 0;
+          for (const val of this.items) {
+             sum += val;
+         }
+         return sum;
+         
+}
+
+
+avg() {
+  let sumNumbers = 0
+  let EmptySortedList = []
+  if (this.items.length === 0) {
+      return EmptySortedList
+  } else {
+  for (const i of this.items) {
+      sumNumbers += i
+ }
+}
+ return sumNumbers / this.items.length
+}
+
 }
 
 module.exports = SortedList;
+
+
+
